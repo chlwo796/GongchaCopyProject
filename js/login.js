d2ee70f3fdfe5dfe4b68a2ca9465671e3a35aa94
chlwo796 <chlwo796@gmail.com>
2023-03-31 11:45:05 +0900

    로그인수정

--- js/login.js
+++ js/login.js
@@ -6,9 +6,9 @@ console.log(url);
 const mainUrl = url.replace("login.html", "main.html");
 console.log(mainUrl);
 // console.log(location.replace("./main.html"));
-let loginButton = document.getElementById("submit"); // 버튼을 만들고.
+// let loginButton = document.getElementById("submit"); // 버튼을 만들고.
 // let login = document.querySelector("button");
-loginButton.addEventListener("click", loginFunc); // 'click'이란 id의 버튼을 누르면 signinFunc 함수를 실행.
+// loginButton.addEventListener("click", loginFunc); // 'click'이란 id의 버튼을 누르면 signinFunc 함수를 실행.
 
 function loginFunc() {
   let userid = document.getElementById("id").value; // userid 의 값을 받아와 넣음.
@@ -16,22 +16,22 @@ function loginFunc() {
 
   if (userid === "chlwo796" && userpw === "12345678") {
     alert("로그인 성공");
-    moveToMain;
+    moveToMain();
     // window.location.href = mainUrl;
-    console.log(location.href);
-    location.replace("./main.html");
+    // console.log(location.href);
+    // location.replace("./main.html");
     // history.back;
     // location.href = "../main.html";
     // const url = window.location.href;
-    console.log(url);
+    // console.log(url);
   } else if (userid === null) {
     alert("아이디를 입력해 주세요.");
   } else {
     alert("비밀번호를 입력해 주세요.");
   }
 }
-// function moveToMain() {
-//   let link = "main.html";
-//   location.href = link;
-// }
+function moveToMain() {
+  let link = "main.html";
+  location.href = link;
+}
 // login.addEventListener("click", moveToMain);
